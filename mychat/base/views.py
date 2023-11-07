0a8b9aeed3bce10e75ffff535e1b147b65ac2b67
Haruto Mori <baystarsman@outlook.jp>
2023-11-07 23:47:48 +0900

    add function to create a new token from agora.io

--- mychat/base/views.py
+++ mychat/base/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from agora_token_builder import RtcTokenBuilder
 
-# Create your views here.
+from decouple import config
+import random
+import time
+
+
+def get_token(request):
+    appId = config('appId')
+    appCertificate = config('appCertificate')
+    channelName = request.GET.get('channel')
+    uid = random.randint(1, 230)
+    expirationTimeInSeconds = 3600 * 24
+    currentTimeStamp = int(time.time())
+    privilegeExpiredTs = currentTimeStamp + expirationTimeInSeconds
+    role = 1
+
+    token = RtcTokenBuilder.buildTokenWithUid(appId, appCertificate, channelName, uid, role, privilegeExpiredTs)
+
+    return JsonResponse({'token': token, 'uid': uid}, safe=False)
+
+
 def lobby(request):
     return render(request, 'base/lobby.html')
 
